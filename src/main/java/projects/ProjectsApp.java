3d6414573f7ea616e0cdd0340c3f8bcefc00b627
Kristen Nardone <you@email.com>
2023-01-28 16:24:27 -0700

    fourth commit

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -14,10 +14,13 @@ public class ProjectsApp {
 	
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService();
+	private Project curProject;
 	
 	//@formatter:off
 	private List<String> operations = List.of(
-			"1) Add a project"
+			"1) Add a project",
+			"2) List projects",
+			"3) Select a project"
 			);
 	//@formatter: on
 	
@@ -44,6 +47,14 @@ public class ProjectsApp {
 			case 1:
 		        createProject();
 		        break;
+		        
+			case 2: 
+				listProjects();
+				break;
+				
+			case 3:
+				selectProject();
+				break;
 				
 			default:
 			    System.out.println("\n:" + selection + "is not a valid selection. Try again.");
@@ -58,7 +69,29 @@ public class ProjectsApp {
 	}
 
 }
-         private void createProject() {
+         private void selectProject() {
+	    listProjects();
+	    Integer projectId = getIntInput("Enter a project ID to select a project");
+	    
+	    curProject = null;
+	    
+	    curProject = projectService.fetchProjectById(projectId);
+            
+	
+	}
+         
+    
+
+         
+		private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		
+		System.out.println("\nProjects:");
+		
+		projects.forEach(project -> System.out.println("   " + project.getProjectId() + ": " + project.getProjectName()));
+	}
+         
+		private void createProject() {
         	 String projectName = getStringInput("Enter the project name");
         	 BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours");
         	 BigDecimal actualHours = getDecimalInput("Enter the actual hours");
@@ -131,6 +164,15 @@ public class ProjectsApp {
 	private void printOperations() {
 		System.out.println("\nThese are the available selections. Press the Enter key to quit");
 		operations.forEach(line -> System.out.println(" " + line));
+		
+	    if(Objects.isNull(curProject)) {
+       	 System.out.println("\nThese are the available selections. Press the Enter key to quit: ");
+       	 
+        }
+        else {
+       	 System.out.println("\nYou are working with project: " + curProject);
+        }
+		
 		
 	}
 }
--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -1,7 +1,12 @@
 package projects.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 public class ProjectService {
 	private ProjectDao projectDao = new ProjectDao();
@@ -10,6 +15,19 @@ public class ProjectService {
 	public Project addProject(Project project) {
 		
 		return projectDao.insertProject(project);
+	}
+
+
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects();
+	}
+
+
+	public Project fetchProjectById(Integer projectId) {
+		return projectDao.fetchProjectById(projectId)
+				.orElseThrow(() -> new NoSuchElementException(
+				"Project with project ID=" + projectId
+				+ " does not exsist."));
 	}
 
 }
